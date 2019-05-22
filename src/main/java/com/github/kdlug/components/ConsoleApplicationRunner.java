4593fb1ed77204cc3b49de85da375cdbd19bf7b8
casi <casi@o2.pl>
2019-05-22 15:15:02 +0200

    Checking arguments

--- src/main/java/com/github/kdlug/components/ConsoleApplicationRunner.java
+++ src/main/java/com/github/kdlug/components/ConsoleApplicationRunner.java
@@ -15,16 +15,24 @@ public class ConsoleApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Application started with command-line arguments: {}", Arrays.toString(args.getSourceArgs()));
+
+        log.info("# NonOptionArgs: " + args.getNonOptionArgs().size());
         log.info("NonOptionArgs: {}", args.getNonOptionArgs());
         log.info("OptionNames: {}", args.getOptionNames());
+
+        log.info("# OptionArgs: " + args.getOptionNames().size());
 
         for (String name : args.getOptionNames()){
             log.info("arg-" + name + "=" + args.getOptionValues(name));
         }
 
         boolean containsOption = args.containsOption("customArgument");
-
         log.info("Contains customArgument: " + containsOption);
+
+        // check if option --customArgument is used
+        if (containsOption) {
+            log.info("customArgument values: " + args.getOptionValues("customArgument"));
+        }
     }
 }
 
